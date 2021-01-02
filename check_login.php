e94e54a46a4ce30e8de9d07b1411b7fc5568354d
raihan <raihanshovon95@gmail.com>
2021-01-02 16:43:56 +0600

    modified login code so it can now detect different examiners by their id

--- check_login.php
+++ check_login.php
@@ -13,28 +13,28 @@
 		echo '<a href="main_login_page.php"><button>BACK</button></a>';
 	}
 
-	//$e_id=$_POST['tid'];
 	$e_username= $_POST['uname'];
 	$e_password= $_POST['pass'];
 	
 	$e_username= stripslashes($e_username);
 	$e_password= stripslashes($e_password);
-	//echo $e_password;
-	//echo "ok";
+
 	$e_username= mysqli_real_escape_string($conn, $e_username);
 	$e_password= mysqli_real_escape_string($conn, $e_password);
 	
 	$e_password_hash= hash('sha512', $e_password);
 	
-	//echo $e_username;
 
-	$query= "SELECT * FROM examiners WHERE username='$e_username' and paswd='$e_password_hash'";
+	$query_1= "SELECT * FROM examiners WHERE username='$e_username' and paswd='$e_password_hash'";
+	$query_2= "SELECT tid FROM examiners WHERE username='$e_username'";
 
-	$result=mysqli_query($conn,$query);
+	$result_1= mysqli_query($conn,$query_1);
 	// Mysql_num_row is counting table row
 	// If result matched $myusername and $mypassword, table row must be 1 row
-	$count=mysqli_num_rows($result);
-	echo "count=".$count;
+	$count=mysqli_num_rows($result_1);
+
+	$result_2= mysqli_query($conn,$query_2);
+	$row=mysqli_fetch_assoc($result_2);
 
 	if($count==1){
 
@@ -42,7 +42,9 @@
 
 		$_SESSION['username']=$e_username;
 		$_SESSION['password']=$e_password_hash;
-		echo"<h1> Welcome $myusername </h1>";
+		$_SESSION['e_id']=$row['tid'];
+
+		//echo"<h1> Welcome $myusername </h1>";
 		header("location:login_success.php");
 	}
 
--- login_success.php
+++ login_success.php
@@ -3,9 +3,18 @@
 // Put this code in first line of web page. 
     session_start();
 
+
 	if(isset($_SESSION['username']) && isset($_SESSION['password'])){
         
         echo "<h1> Sucessfully login </h1>";
+        /*$e=$_SESSION['e_id'];
+
+        if($e==1){
+        	echo "gogo";
+        }
+        if($e==2){
+        	echo "NOPNO";
+        }*/
         
         echo '<a href="result_submission_c1.php"><button>Proceed</button></a>';
 	} 
